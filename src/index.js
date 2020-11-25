54dea00ba5d1573c4d520dc1c0edd5299b40aec4
童雅莉 <1046840300@qq.com>
2020-11-25 14:05:30 +0800

    自动打包

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
 $(function () {
-  $('li:odd').css('backgroundColor', 'pink')
-  $('li:even').css('backgroundColor', 'green')
+  $('li:odd').css('backgroundColor', 'skyblue')
+  $('li:even').css('backgroundColor', 'pink')
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+const path = require('path')
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  // entry: path.join(__dirname, './', 'src', '/', 'index.js'),
+  // output: {
+  //   path: path.join(dirname, './', 'dist'),
+  //   filename: 'bundle.js'
+  // },
+  entry: path.join(__dirname, "./src/index.js"),
+  // 设置出口文件
+  output: {
+    // 设置路径
+    path: path.join(__dirname, "./dist"),
+    // 设置文件名
+    filename: "bundle.js"
+  }
 }
